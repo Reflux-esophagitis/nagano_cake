4a0c4cd2bf93d354bdf122a0e7bebe6a15bc8ee3
usausa <143808163+uasuas@users.noreply.github.com>
2023-11-13 11:31:35 +0000

    [update] 各item_controller 各item/index編集

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,41 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item)
+    else
+      redirect_to new_admin_item_path
+    end
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update
+      redirect_to admin_item_path(@item)
+    else
+      redirect_to edit_admin_item_path(@item)
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :non_taxed_price, :introduction, :is_active, :image)
   end
 end
