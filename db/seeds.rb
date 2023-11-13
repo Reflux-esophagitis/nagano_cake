682df360d10e10ea8a134be17828c6b7831a835e
Masato <144527087+3-masato@users.noreply.github.com>
2023-11-13 11:56:40 +0000

    [add] 管理者ログイン・ログアウト機能実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ADMIN_TEST_EMAIL = "admin@admin"
+ADMIN_TEST_PASSWORD = "adminadmin"
+
+Admin.create!(
+  email: ADMIN_TEST_EMAIL,
+  encrypted_password: ADMIN_TEST_PASSWORD,
+  password: ADMIN_TEST_PASSWORD,
+  password_confirmation: ADMIN_TEST_PASSWORD,
+)
+puts "[Seeds] Created Admin user"
