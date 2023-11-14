08321d9eb83fd4c7aca4c839bb2571fffa46e47f
Keishiro Takata <t.k.6.2.1995@gmail.com>
2023-11-14 07:12:40 +0000

    [add] confirm view

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,23 +8,29 @@ class Public::OrdersController < ApplicationController
     @order_items = current_customer.cart_items
     @order = Order.new(order_params)
     @order.customer_id = current_customer.id
-    case address_method
+    case params[:address_method]
     when 0
       @order.zip_code = current_customer.zip_code
       @order.address = current_customer.address
       @order.name = current_customer.name
     when 1
       registered_address = customer.addresses.find_by(params[address_id])
+      @order.zip_code = registered_address.zip_code
+      @order.address = registered_address.address
+      @order.name = registered_address.name
     when 2
       if params[:zip_code].blank? || params[:address].blank? || params[:name].blank?
         flash[:error] = "配送先が入力されていません"
         redirect_to new_order_path
       else
-        @order.postal_code = params[:order][:postal_code]
-        @order.address = params[:order][:address]
-        @order.name = params[:order][:name]
+        new_address = current_customer.address.create({
+          zip_code: params[zip_code],
+          address: params[address],
+          name: params[:name]
+        })
       end
     end
+    
   end
 
   def complete
@@ -38,6 +44,6 @@ class Public::OrdersController < ApplicationController
 
   private
     def order_params
-      params.require(:order).permit(:name, :zip_code, :address, :payment_method, :address_method)
+      params.require(:order).permit(:name, :zip_code, :address, :payment_method)
     end
 end
