2bd22e8ed9cfc27de5b1fcda3aa98fd0952afe24
Masato <144527087+3-masato@users.noreply.github.com>
2023-11-14 10:17:53 +0000

    [add] 小計を求めるメソッドを追加

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,10 @@
 class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
+
+  def subtotal
+    price = item.non_taxed_price
+
+    ((price * amount) * (1 + TAX_RATE)).truncate
+  end
 end
