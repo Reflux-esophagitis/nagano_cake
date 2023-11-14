9004d556846141d4a809d1d85cb584dda9da647f
Keishiro Takata <t.k.6.2.1995@gmail.com>
2023-11-14 01:28:35 +0000

    [update] customer-signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   private
   def after_sign_in_path_for(resource)
     case resource
@@ -17,4 +19,12 @@ class ApplicationController < ActionController::Base
       root_path
     end
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :zip_code, :address, :tel, :is_active ])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+  end
+
 end
