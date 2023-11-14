a0b2d5bc8bd1235ed146b68f0dca92a5cb4e036d
usausa <143808163+uasuas@users.noreply.github.com>
2023-11-14 05:56:12 +0000

    [fix] スペルミス修正

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,10 +1,10 @@
 class Public::CartItemsController < ApplicationController
   def index
   end
-  
+
   def create
     cart_item = CartItem.new(cart_item_params)
-    cart_item.customer_id = current_costomer.id
+    cart_item.customer_id = current_customer.id
     cart_item.item_id = cart_item_params[:item_id]
     # カート内に存在するなら
     if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
@@ -16,11 +16,11 @@ class Public::CartItemsController < ApplicationController
     else
       cart_item.save
       redirect_to cart_items_path
-      
+    end
   end
-  
+
   private
-  
+
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount)
   end
