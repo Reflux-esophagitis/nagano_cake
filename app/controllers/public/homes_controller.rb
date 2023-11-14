369447b096da913290118e1849967ffec348cace
Masato <144527087+3-masato@users.noreply.github.com>
2023-11-14 07:44:47 +0000

    [update] 新着商品を取得するように更新

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,8 @@
 class Public::HomesController < ApplicationController
+  DISPLAY_NEW_ITEMS_COUNT = 4
+
   def top
-    # TODO
-    # 全商品ではなく、新着商品を取得する
-    @items = Item.all
+    @new_items = Item.active.order(created_at: :desc).limit(DISPLAY_NEW_ITEMS_COUNT)
     @genres = Genre.all
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,6 @@ class Item < ApplicationRecord
   belongs_to :genre
 
   has_one_attached :image
-  
+
+  scope :active, -> { where(is_active: true) }
 end
