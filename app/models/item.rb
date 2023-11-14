dba2fb170fd69b8f093ef856babec06dea128990
usausa <143808163+uasuas@users.noreply.github.com>
2023-11-14 12:36:06 +0000

    [update] admin/item/newの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,10 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_one_attached :image
 
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :non_taxed_price, presence: true
+  validates :is_active, presence: true
+
   scope :active, -> { where(is_active: true) }
 end
