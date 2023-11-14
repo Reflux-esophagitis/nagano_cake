ef8643389faa7687a3c279e0ad0c7587dc3e4da6
Keishiro Takata <t.k.6.2.1995@gmail.com>
2023-11-14 06:57:40 +0000

    [update] controller

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,6 +6,25 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order_items = current_customer.cart_items
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    case address_method
+    when 0
+      @order.zip_code = current_customer.zip_code
+      @order.address = current_customer.address
+      @order.name = current_customer.name
+    when 1
+      registered_address = customer.addresses.find_by(params[address_id])
+    when 2
+      if params[:zip_code].blank? || params[:address].blank? || params[:name].blank?
+        flash[:error] = "配送先が入力されていません"
+        redirect_to new_order_path
+      else
+        @order.postal_code = params[:order][:postal_code]
+        @order.address = params[:order][:address]
+        @order.name = params[:order][:name]
+      end
+    end
   end
 
   def complete
@@ -19,6 +38,6 @@ class Public::OrdersController < ApplicationController
 
   private
     def order_params
-      params.require(:order).permit(:name, :zip_code, :address, :payment_method)
+      params.require(:order).permit(:name, :zip_code, :address, :payment_method, :address_method)
     end
 end
