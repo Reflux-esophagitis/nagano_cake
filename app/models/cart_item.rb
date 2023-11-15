74f3d7a7d913838c5d1f69c828f70b77f0de985c
Masato <144527087+3-masato@users.noreply.github.com>
2023-11-15 07:01:27 +0000

    [add] CartItem.total_priceのコメントを追記

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -8,6 +8,14 @@ class CartItem < ApplicationRecord
     (price * amount) * (1 + TAX_RATE)
   end
 
+  # カート内の各アイテムについて、subtotalメソッドを呼び出して各アイテムの小計を求め、
+  # それらの小計を合計してカート内の総額を計算します。
+  #
+  # `map`メソッドは各アイテムのsubtotalを配列として収集し、
+  # `sum`メソッドはその配列の要素（各小計）を合計します。
+  #
+  # `&:subtotal`はRubyのシンタックスシュガーで、以下のコードと同じです。
+  # `cart_items.map { |item| item.subtotal }.sum`
   def self.total_price(cart_items)
     cart_items.map(&:subtotal).sum
   end
