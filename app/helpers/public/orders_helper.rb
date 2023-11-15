b4fe13c6415417cc0e3cf3f7e27029d7524beecb
Keishiro Takata <t.k.6.2.1995@gmail.com>
2023-11-15 05:44:28 +0000

    [fix] methodの不要な引数を削除-2

--- app/helpers/public/orders_helper.rb
+++ app/helpers/public/orders_helper.rb
@@ -1,16 +1,16 @@
 module Public::OrdersHelper
 
 # num導入後に不要ならば削除
-  def payment_method(args)
-    if args.payment_method == 0
+  def payment_method
+    if self.payment_method == 0
       "クレジットカード"
-    elsif args.payment_method == 1
+    elsif self.payment_method == 1
       "銀行振込"
     end
   end
 
-  def full_address(args)
-    "#{args.zip_code} #{args.address}"
+  def full_address
+    "#{self.zip_code} #{self.address}"
   end
 
 end
