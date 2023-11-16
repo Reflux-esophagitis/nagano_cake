9e9cdb46b82d24dee125ba0f1a0be82023cb801b
Keishiro Takata <t.k.6.2.1995@gmail.com>
2023-11-16 05:23:08 +0000

    [update] address_index

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,34 @@
 class Public::AddressesController < ApplicationController
   def index
+    @addresses = current_customer.addresses
+    @address = Address.new
+  end
+
+  def create
+    @address = current_customer.addresses.new(address_params)
+    if @address.save
+      redirect_to index
+    else
+      @addresses = current_customer.addresses
+      render :index
+    end
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
+
+  def update
+     @address = Address.find(params[:id])
+    if @address.update(order_params)
+      redirect_to index
+    else
+      render :edit
+    end
+  end
+
+  private
+    def address_params
+      params.require(:address).permit(:zip_code, :address, :name)
+    end
 end
