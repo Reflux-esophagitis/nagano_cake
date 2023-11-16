2d76f60f3da215d71e319ea8ad7c2168672f927f
Keishiro Takata <t.k.6.2.1995@gmail.com>
2023-11-16 05:59:18 +0000

    [update] address_index_add_edit

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,4 +1,6 @@
 class Public::AddressesController < ApplicationController
+  before_action :is_matching_address_customer, only: %i[edit update destroy]
+
   def index
     @addresses = current_customer.addresses
     @address = Address.new
@@ -20,15 +22,27 @@ class Public::AddressesController < ApplicationController
 
   def update
      @address = Address.find(params[:id])
-    if @address.update(order_params)
-      redirect_to index
+    if @address.update(address_params)
+      redirect_to addresses_path
     else
       render :edit
     end
   end
 
+  def destroy
+    @address = Address.find(params[:id]).destroy
+    redirect_to addresses_path
+  end
+
   private
     def address_params
       params.require(:address).permit(:zip_code, :address, :name)
+    end
+
+    def is_matching_address_customer
+      customer = Address.find(params[:id]).customer
+      if customer != current_customer
+        redirect_to addresses_path
+      end
     end
 end
--- app/helpers/public/addresses_helper.rb
+++ app/helpers/public/addresses_helper.rb
@@ -1,2 +1,7 @@
 module Public::AddressesHelper
+
+  def address_btn_text
+    current_page?(addresses_path) ? "新規登録" : "変更内容を保存"
+  end
+
 end
