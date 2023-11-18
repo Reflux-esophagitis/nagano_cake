d2e698b3603a55f8042d58e7881cb52589b3483c
Masato <144527087+3-masato@users.noreply.github.com>
2023-11-18 06:11:15 +0000

    [add] ジャンル編集機能を実装

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,5 +1,6 @@
 class Admin::GenresController < ApplicationController
   before_action :authenticate_admin!
+  before_action :set_genre, only: %i[edit update]
 
   def index
     @genres = Genre.all
@@ -20,10 +21,32 @@ class Admin::GenresController < ApplicationController
   end
 
   def update
+    old_name = @genre.name
+
+    if @genre.update(genre_params)
+      new_name = @genre.name
+
+      notice = updated_notice_text(old_name, new_name)
+      redirect_to admin_genres_path, notice: notice
+    else
+      render :edit
+    end
   end
 
   private
   def genre_params
     params.require(:genre).permit(:name)
+  end
+
+  def set_genre
+    @genre = Genre.find(params[:id])
+  end
+
+  def updated_notice_text(old_name, new_name)
+    if old_name != new_name
+      "ジャンル名を「#{old_name}」から「#{new_name}」へ変更しました。"
+    else
+      "ジャンル名に変更はありませんでした。"
+    end
   end
 end
