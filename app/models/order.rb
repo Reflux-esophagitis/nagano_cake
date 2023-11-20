f4c0362b3eb917e9fabe9709ae5a5de6144fa6aa
Masato <144527087+3-masato@users.noreply.github.com>
2023-11-20 03:05:05 +0000

    [update] Orderモデルにバリデーション設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,14 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
 
+  validates :name, presence: true
+  validates :zip_code, presence: true
+  validates :address, presence: true
+  validates :payment_method, presence: true, inclusion: { in: [0, 1], message: "は0, 1のいずれかでなければなりません" }
+  validates :status, presence: true, inclusion: { in: [0, 1, 2, 3, 4], message: "は0, 1, 2, 3, 4のいずれかでなければなりません" }
+  validates :total_price, presence: true
+  validates :postage, presence: true
+
   def full_address
     "#{self.zip_code} #{self.address}"
   end
@@ -9,5 +17,4 @@ class Order < ApplicationRecord
   def complete_all_details?
     order_details.all? { |detail| detail.status == 3 }
   end
-
 end
