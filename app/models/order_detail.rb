2430d311ec28e2079c2e0ed303b4ae4b177b6da6
Masato <144527087+3-masato@users.noreply.github.com>
2023-11-20 03:26:45 +0000

    [update] OrderDetailモデルのバリデーション設定

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,8 @@
 class OrderDetail < ApplicationRecord
   belongs_to :item
   belongs_to :order
+
+  validates :price, presence: true
+  validates :amount, presence: true
+  validates :status, presence: true, inclusion: { in: [0, 1, 2, 3] }
 end
