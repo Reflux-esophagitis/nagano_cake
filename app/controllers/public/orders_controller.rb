22d90a563a53448f9109d539ee49ed06a916a69c
Keishiro Takata <t.k.6.2.1995@gmail.com>
2023-11-21 02:35:17 +0000

    [update] order_confirmの内部をメソッド化

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -12,36 +12,13 @@ class Public::OrdersController < ApplicationController
     @order.customer_id = current_customer.id
     @order.payment_method = params[:order][:payment_method].to_i
     @order.postage = POSTAGE
-    case params[:order][:address_method].to_i
-    when 0
-      @order.zip_code = current_customer.zip_code
-      @order.address = current_customer.address
-      @order.name = current_customer.full_name
-    when 1
-      registered_address = current_customer.addresses.find(params[:order][:address_id])
-      @order.zip_code = registered_address.zip_code
-      @order.address = registered_address.address
-      @order.name = registered_address.name
-    when 2
-      if params[:order][:zip_code].blank? || params[:order][:address].blank? || params[:order][:name].blank?
-        # flash[:error] = "配送先が入力されていません"
-        redirect_to new_order_path
-      else
-        @order.zip_code = params[:order][:zip_code]
-        @order.address = params[:order][:address]
-        @order.name = params[:order][:name]
-        current_customer.addresses.create({
-          zip_code: params[:order][:zip_code],
-          address: params[:order][:address],
-          name: params[:order][:name]
-        })
-      end
-    end
+    set_address
   end
 
   def create
     @order = Order.new(order_params)
     @order.save
+    byebug
     create_details(@order)
     redirect_to complete_orders_path
   end
@@ -71,6 +48,10 @@ class Public::OrdersController < ApplicationController
         :postage)
     end
 
+    def address_params
+      params.require(:order).permit(:zip_code, :address, :name)
+    end
+
     # カートアイテムを注文詳細に変換
     # その後ログインユーザーのカートアイテムを全削除
     # トランザクションにて問題発生時にカートアイテムを消さない
@@ -86,6 +67,45 @@ class Public::OrdersController < ApplicationController
         end
         cart_items.destroy_all
       end
+    end
+
+    # オーダー登録画面にて選択された住所から条件分岐
+    # 0:ユーザーの登録情報を配送先として使用
+    # 1:ユーザーが以前登録した配送先を使用
+    # 2:新規住所を登録して配送先として使用
+    def set_address
+      case params[:order][:address_method].to_i
+      when 0
+        set_address_from_customer
+      when 1
+        set_address_from_registered
+      when 2
+        set_address_from_params
+      end
+    end
+
+    def set_address_from_customer
+      assign_address(current_customer.zip_code, current_customer.address, current_customer.full_name)
+    end
+
+    def set_address_from_registered
+      registered_address = current_customer.addresses.find(params[:order][:address_id])
+      assign_address(registered_address.zip_code, registered_address.address, registered_address.name)
+    end
+
+    def set_address_from_params
+      if params[:order][:zip_code].blank? || params[:order][:address].blank? || params[:order][:name].blank?
+        redirect_to new_order_path
+      else
+        assign_address(params[:order][:zip_code], params[:order][:address], params[:order][:name])
+        current_customer.addresses.create(address_params)
+      end
+    end
+
+    def assign_address(zip_code, address, name)
+      @order.zip_code = zip_code
+      @order.address = address
+      @order.name = name
     end
 
 end
