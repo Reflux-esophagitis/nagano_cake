873f95a15f61a9f4245ecc7ff5d1d48c9205995a
usausa <143808163+uasuas@users.noreply.github.com>
2023-11-21 04:39:08 +0000

    [update] flashメッセージ非同期化

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -38,13 +38,13 @@ class Public::CartItemsController < ApplicationController
     @cart_items = current_customer.cart_items
     @cart_item = current_customer.cart_items.find_by(item_id: params[:id])
     if @cart_item
-      # item_name = cart_item.item.name
+      @item_name = @cart_item.item.name
       @cart_item.destroy
-      # redirect_to cart_items_path, notice: "#{item_name}を削除しました。"
+      flash.now[:notice] = "#{@item_name}を削除しました。"
       render :cart_table
     else
       # カート内アイテムが見つからなかったときの処理
-      # redirect_to cart_items_path, alert: "選択した商品が見つかりませんでした。"
+      flash.now[:alert] = "選択した商品が見つかりませんでした。"
       render :cart_table
     end
   end
