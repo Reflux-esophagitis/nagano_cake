c4bc897a5f6f058e04f78a7fcb831b5c7c3d88a1
Masato <144527087+3-masato@users.noreply.github.com>
2023-11-23 07:09:11 +0000

    [update] 製作ステータス更新時のフラッシュメッセージを追加

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,7 +3,7 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     order_detail = OrderDetail.find(params[:id])
     order_detail.update(status: params[:order_detail][:status].to_i)
-    redirect_to admin_order_path(order_detail.order)
+    redirect_to admin_order_path(order_detail.order), notice: "製作ステータスを更新しました。"
   end
 
 end
