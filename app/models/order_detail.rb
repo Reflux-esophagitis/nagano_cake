36663cfb4bcff05e707e4306c6ecf1ed525045dd
Masato <144527087+3-masato@users.noreply.github.com>
2023-11-23 13:56:19 +0000

    [update] とをConcernを使用して共通化

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,5 @@
 class OrderDetail < ApplicationRecord
+  include ItemAmountCalculations
 
   enum status: {
     not_producible: 0,
@@ -12,6 +13,5 @@ class OrderDetail < ApplicationRecord
 
   validates :price, presence: true
   validates :amount, presence: true
-  validates :status, presence: true, inclusion: {in: OrderDetail.statuses.keys}
-
+  validates :status, presence: true, inclusion: { in: statuses.keys }
 end
