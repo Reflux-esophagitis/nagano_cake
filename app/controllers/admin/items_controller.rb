c08ea03d723d00901bba666d6efb48bdc1d2a943
Masato <144527087+3-masato@users.noreply.github.com>
2023-11-24 10:02:00 +0000

    [update] フラッシュメッセージの追加とリファクタリング

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,4 +1,7 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :set_item, only: %i[show edit update]
+
   def index
     @items = Item.all.with_genre.page(params[:page])
   end
@@ -10,32 +13,41 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to admin_item_path(@item)
+      name = @item.name
+      redirect_to admin_item_path(@item), "新しい商品「#{name}」を追加しました。"
     else
       render :new
     end
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to admin_item_path(@item)
+      name = @item.name
+      redirect_to admin_item_path(@item), notice: "#{name}の情報を更新しました。"
     else
       render :edit
     end
   end
 
   private
-
   def item_params
-    params.require(:item).permit(:genre_id, :name, :non_taxed_price, :introduction, :is_active, :image)
+    params.require(:item).permit(
+      :image,
+      :name,
+      :introduction,
+      :genre_id,
+      :non_taxed_price,
+      :is_active
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
