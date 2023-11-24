c9ee3c2c1b64ef19507892ed85212036a7b50439
usausa <143808163+uasuas@users.noreply.github.com>
2023-11-24 05:03:50 +0000

    [update] 条件内容変更/フラッシュ実装

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -9,18 +9,22 @@ class Admin::OrderDetailsController < ApplicationController
       flash[:alert] = "製作ステータスが発送済みのため、注文ステータスを変更できません。"
     # 製作ステータスが製作中の場合
     elsif new_status == 2
+      flash[:notice] = "製作ステータスを更新しました。"
       order_detail.update(status: params[:order_detail][:status].to_i)
       # 一つでもあれば注文ステータスを製作中に変更
-      if order.order_details.any? { |order_detail| order_detail.status == 'in_production' }
+      if order.order_details.any? { |order_detail| order_detail.status == "in_production" }
         order.update(status: 2)
       end
+    elsif new_status == 3
+      flash[:notice] = "製作ステータスを更新しました。"
+      order_detail.update(status: params[:order_detail][:status].to_i)
       # 全てであれば注文ステータスを発送準備中に変更
-      if order.order_details.all? { |order_detail| order_detail.status == 'in_production' }
+      if order.order_details.all? { |order_detail| order_detail.status == "production_completed" }
         order.update(status: 3)
       end
     else
       # それ以外の場合
-      flash[:notice] = "更新に成功しました。"
+      flash[:notice] = "製作ステータスを更新しました。"
       order_detail.update(status: params[:order_detail][:status].to_i)
     end
     redirect_to admin_order_path(order_detail.order)
