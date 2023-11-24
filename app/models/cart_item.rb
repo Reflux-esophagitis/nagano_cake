3b24ce54966b5741e1dd0fccd9198fe1c02a28c4
Masato <144527087+3-masato@users.noreply.github.com>
2023-11-24 03:40:57 +0000

    [fix] コンフリクト修正時のミス修正

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -6,5 +6,6 @@ class CartItem < ApplicationRecord
 
   validates :amount, numericality: { only_integer: true, greater_than: 0 }
 
+  # カートアイテムデータ内の商品情報とその画像を事前に読み込む
   scope :with_items_and_images, -> { includes(item: { image_attachment: :blob }) }
 end
